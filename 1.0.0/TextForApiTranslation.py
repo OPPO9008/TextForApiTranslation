bc6adccc3e2c8460d3db88c1f8a51f931c1696a2
OPPO9008 <41640509+OPPO9008@users.noreply.github.com>
2023-05-02 14:47:53 +0800

    Update TextForApiTranslation.py
    彩云小译

--- 1.0.0/TextForApiTranslation.py
+++ 1.0.0/TextForApiTranslation.py
@@ -1,33 +1,53 @@
 #!/usr/bin/python3
 #coding:utf-8
 from json import loads
-from requests import post
+import requests
+import json
 from ast import literal_eval
 from time import sleep
 
+
 def PostApi(key):#Post Api
-    ApiKey = '输入你的小牛ApiKey'
-    url = 'https://api.niutrans.com/NiuTransServer/translation'
-    date = {'src_text':key,'from':'auto','to':'zh','apikey':ApiKey}#ApiKEY
-    ApiDate = post(url,date)
-    #print(ApiDate.text)
-    if ApiDate.status_code == 200:
-        sleep(0.05)
-        return literal_eval(ApiDate.text)['tgt_text']
+    ApiKey = '3975l6lr5pcbvidl6jl2'
+    url = 'http://api.interpreter.caiyunai.com/v1/translator'
+    payload = {
+            "source" : key, 
+            "trans_type" : "auto2zh",
+            "request_id" : "demo",
+            "detect": True
+            }
+    headers = {
+            'content-type': "application/json",
+            'x-authorization': "token 3975l6lr5pcbvidl6jl2"
+    }
+    while True:
+        ApiDate = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+        if ApiDate.status_code == 200:
+            print(json.loads(ApiDate.text)["target"])
+            return json.loads(ApiDate.text)["target"]
+            break
+        else :
+            sleep(5)
 
 def TextSegment(DateKeys):#文本分段
-    i = len(DateKeys)
+    i = len(DateKeys)#获取待翻译文本元素数
+    start = 0
     Keys = ''
-    Dkeys = []
+    fen_duan = {}
+    
     for i in range(i):
-        Keys = Keys + DateKeys[i].replace('\n','^C^') + '\n'
-        if len(Keys) < 4500:
+        Keys = Keys + DateKeys[i] 
+        if len(Keys) < 3500:
             Keys1 = Keys
         else :
+            fen_duan[start] = i
             Keys = ''
-            Keys = Keys + DateKeys[i].replace('\n','^C^') + '\n'
-            Dkeys.append(Keys1.strip('\n'))
-    return Dkeys
+            start = i
+            
+    fen_duan[start] = i 
+    print(fen_duan)
+    
+    return fen_duan
 
 def ReadTransFile():#读取
     with open(f"{Path}\ManualTransFile.json",'r',encoding='utf-8') as ff:
@@ -39,15 +59,16 @@ def WriteTransFile(json):#写入
 
 def DealWithApiDate(Date):#处理数据
     DateKeys = list(Date.keys())
-    text = ''
-    DateKeysS = TextSegment(DateKeys)#分段以进行加速翻译避免QPS超标
-    for i in range(len(DateKeysS)):
-        print(f'[i]正在进行{len(DateKeysS)}个分段中的{i+1}')
-        text = text + (PostApi(DateKeysS[i]))
-    text = text.split('\n')
+    text = []
+    fen_duan = TextSegment(DateKeys)#分段字典
+    for i in range(len(fen_duan)):
+        print(f'[i]正在进行{len(fen_duan)}个分段中的{i+1}')
+        stat = list(fen_duan)[i]
+        end  = fen_duan[stat]
+        text += (PostApi(DateKeys[stat:end]))
     for i in range(len(text)):
-        Date[DateKeys[i]] = text[i].replace('^C^','\n')
-    WriteTransFile(Date)
+        Date[DateKeys[i]] = text[i]
+    WriteTransFile(json.dumps(Date,ensure_ascii=False))
     print('[i]all ok')
 
 if __name__ == '__main__' :
